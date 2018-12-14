7b8c6ad51667f727ace28ad2c2a8132bb3ee7169
Fajri Abdillah <clasense4@gmail.com>
2018-12-15 06:48:04 +0700

    Update admin diplay

--- dagangweb/backend/admin.py
+++ dagangweb/backend/admin.py
@@ -3,6 +3,16 @@ from django.contrib import admin
 # Register your models here.
 from .models import User
 from .models import Product
+from .models import Cart
 
-admin.site.register(User)
-admin.site.register(Product)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'telegram_id', 'register_date')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product', 'quantity')
