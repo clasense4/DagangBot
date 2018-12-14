09cfb57ce23991edcb745251a6731462b35ac809
Fajri Abdillah <clasense4@gmail.com>
2018-12-15 06:47:44 +0700

    Update models

--- dagangweb/backend/models.py
+++ dagangweb/backend/models.py
@@ -6,8 +6,22 @@ class User(models.Model):
     telegram_id = models.TextField(null=True)
     first_name = models.TextField(null=True)
     last_name = models.TextField(null=True)
-    register_date = models.TextField(null=True)
+    register_date = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return '%s %s' % (self.first_name, self.last_name)
 
 class Product(models.Model):
     name = models.TextField(null=True)
-    price = models.TextField(null=True)
+    price = models.BigIntegerField(null=False)
+
+    def __str__(self):
+        return '%s' % (self.name)
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False)
+
+    def __str__(self):
+        return '%s %s, %s : %s' % (self.user.first_name, self.user.last_name, self.product.name, self.quantity)
